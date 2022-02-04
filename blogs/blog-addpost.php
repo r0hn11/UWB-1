7165e0c5afa741981873ba36ebe2345ecbba576a
Shaswat Samaddar <92857094+samaddarshaswat@users.noreply.github.com>
2022-02-04 21:53:39 +0530

    Update blog-addpost.php
    Blog insertion by user

--- blogs/blog-addpost.php
+++ blogs/blog-addpost.php
@@ -1,6 +1,5 @@
 <?php
-
-
+    session_start();
     $host="localhost";
     $username="root";
     $pass="";
@@ -9,20 +8,17 @@
     if(!$conn){
         die("Connection failed: " . mysqli_connect_error());
     }
-
-$a_title = $_POST["a_title"];
-$a_desc = $_POST["a_desc"];
-$a_story = $_POST["a_story"];
-$a_tags = $_POST["a_tags"];
-$date = date("Y-m-d");
-
- $sql = "insert into blog(bid,uid,uname,btitle, bdescription, bstory, btags,bstatus,bviewed,comment,date) values('','11','test1','$a_title','$a_desc','$a_story','$a_tags','0','0','','$date')";
-
- if(mysqli_query($conn,$sql)){
-     echo 1;
- } else{
-     echo 0;
-     
- }
-
+    $usignnm = $_SESSION["usignnm"];
+    $a_title = mysqli_real_escape_string($conn,$_POST["a_title"]);
+    $a_desc =  mysqli_real_escape_string($conn,$_POST["a_desc"]); 
+    $a_story = mysqli_real_escape_string($conn,$_POST["a_story"]); 
+    $a_tags =  mysqli_real_escape_string($conn,$_POST["a_tags"]); 
+    $date = date("Y-m-d");
+    $sql = 'insert into blog(bid,uid,uname,btitle,bdescription,bstory,btags,bstatus,bviewed,comment,date) values("","","'.$usignnm.'","'.$a_title.'","'.$a_desc.'","'.$a_story.'","'.$a_tags.'","0","0","","'.$date.'")';
+    if(mysqli_query($conn,$sql)){
+        echo 1;
+    }
+    else{
+        echo 0;
+    }
 ?>
